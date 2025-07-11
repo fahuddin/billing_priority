f48b89009d7223bdae5564a985f11fb1bddac865
fahuddin <fahuddin@bu.edu>
2025-07-11 15:44:46 -0400

    added evaluate_model

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,4 +1,12 @@
 from sklearn.metrics import classification_report, confusion_matrix
 
 def evaluate_model(model, X, y_true):
-    pass
+    y_pred = model.predic(X)
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_true, y_pred))
+    
+    print("\nClassification Report:")
+    report = classification_report(y_true, y_pred, output_dict=False)
+    print(report)
+    
+    return report
